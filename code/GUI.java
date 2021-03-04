80ec0b93cb3b263cc70c1cab32d54ebdf91ab2dd
Jurgen van den Berg <jurgenberg4@gmail.com>
2021-03-04 16:53:27 +0100

    code GUI aangepast

--- code/GUI.java
+++ code/GUI.java
@@ -2,15 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/*
+    na pas inserten -- meteen inloggen
+    na inloggen -- saldo, pinnen, snelkeuze 70eu, 
+
+    altijd taal kunnen veranderen en kunnen afbreken
+
+
+*/
+
 public class GUI extends JFrame implements ActionListener {
     private int breedte = 600, hoogte = 800;
-    static private int gridH = 4, gridW = 3;
     private JButton[] knoppen;
     private JPanel blad;
     private Graphics g;
 
     GUI() {
-        knoppen = new JButton[gridH * gridW];
+        knoppen = new JButton[8];
         setSize(breedte, hoogte);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setUndecorated(true);
@@ -26,21 +34,45 @@ public class GUI extends JFrame implements ActionListener {
 
     private void createGUI() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        Container venster = this.getContentPane();
-        //venster.setLayout(new GridLayout(gridH, gridW));
-        // for(int i = 0; i < gridW; i++) {
-        //     for(int j = 0; j < gridH - 1; j++) {
-        //         knoppen[i] = new JButton(String.valueOf(i * (gridH - 1) + j + 1));
-        //         venster.add(knoppen[i]);
-        //         knoppen[i].addActionListener(this);
-        //     }
-        // }
+        JPanel venster = new JPanel();
+        JPanel vensterLinks = new JPanel();
+        JPanel vensterRechts = new JPanel();
+        getContentPane().setLayout(new BorderLayout(50, 50));
+        vensterLinks.setLayout(new GridLayout(4 , 1, 50, 50));
+        vensterLinks.setPreferredSize(new Dimension(300, MAXIMIZED_VERT));
+        vensterRechts.setLayout(new GridLayout(4, 1, 50, 50));
+        vensterRechts.setPreferredSize(new Dimension(300, MAXIMIZED_VERT));
+
         knoppen[0] = new JButton("1");
-        knoppen[1] = new JButton("2");
-        venster.add(knoppen[0], BorderLayout.WEST);
+        vensterLinks.add(knoppen[0]);
         knoppen[0].addActionListener(this);
-        venster.add(knoppen[1], BorderLayout.EAST);
+        knoppen[1] = new JButton("2");
+        vensterLinks.add(knoppen[1]);
         knoppen[1].addActionListener(this);
+        knoppen[2] = new JButton("3");
+        vensterLinks.add(knoppen[2]);
+        knoppen[2].addActionListener(this);
+        knoppen[3] = new JButton("4");
+        vensterLinks.add(knoppen[3]);
+        knoppen[3].addActionListener(this);
+
+        knoppen[4] = new JButton("5");
+        vensterRechts.add(knoppen[4]);
+        knoppen[4].addActionListener(this);
+        knoppen[5] = new JButton("5");
+        vensterRechts.add(knoppen[5]);
+        knoppen[5].addActionListener(this);
+        knoppen[6] = new JButton("5");
+        vensterRechts.add(knoppen[6]);
+        knoppen[6].addActionListener(this);
+        knoppen[7] = new JButton("5");
+        vensterRechts.add(knoppen[7]);
+        knoppen[7].addActionListener(this);
+
+
+
+        getContentPane().add(vensterLinks, BorderLayout.WEST);
+        getContentPane().add(vensterRechts, BorderLayout.EAST);
         blad = new JPanel();
         venster.add(blad, BorderLayout.CENTER);
     }
