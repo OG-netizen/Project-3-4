f76e2fcbaf3631e199334c300037df29a9d5e614
OG-netizen <70165988+OG-netizen@users.noreply.github.com>
2021-03-04 23:09:48 +0100

    Update Authentication.java

--- code/Authentication.java
+++ code/Authentication.java
@@ -1,9 +1,15 @@
+// Set jou passcode
 
 public class Authentication {
 	
 	private String correctPIN="1234";
 	public boolean authenticate(String pin) {
 		return(pin.equals(correctPIN));
+	}
+	
+	private void setCorrectPIN(int pin){
+	this.correctPIN = pin;
+		
 	}
 
 }
