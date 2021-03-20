72cdad4962d1838f366147f319b54e62f2c2c2dd
Bryan <chungchakho@gmail.com>
2021-03-20 21:49:43 +0100

    code aangepast

--- DemoSerialMonitor/src/demo.java
+++ DemoSerialMonitor/src/demo.java
@@ -1,72 +1,81 @@
 
-import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortException;
-import jssc.SerialPortList;
+import java.io.*; // IOException
+import java.util.*; // Scanner
+import jssc.*;
 
-public class demo {
-	
+public class ComPortSendReceive {
 
-	public void connect(String portname) {
-		
-		SerialPort port = new SerialPort(portname);
-		
-		try {
-			port.openPort();
-			
-			port.setParams(
-					
-					SerialPort.BAUDRATE_9600,
-					SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE
-					);
-			port.addEventListener((SerialPortEvent event)->{
-				
-				if(event.isRXCHAR()) {
-					
-					try {
-						String s = port.readString();
+    private static SerialPort serialPort;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] portNames = SerialPortList.getPortNames();
+        
+        if (portNames.length == 0) {
+            System.out.println("There are no serial-ports :( You can use an emulator, such ad VSPE, to create a virtual serial port.");
+            System.out.println("Press Enter to exit...");
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return;
+        }
+        
+        
+        System.out.println("Available com-ports:");
+        for (int i = 0; i < portNames.length; i++){
+            System.out.println(portNames[i]);
+        }
+        System.out.println("Type port name, which you want to use, and press Enter...");
+        Scanner in = new Scanner(System.in);
+        String portName = in.next();
+        
+        // writing to port
+        serialPort = new SerialPort(portName);
+        try {
+            // opening port
+            serialPort.openPort();
+            
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                                 SerialPort.DATABITS_8,
+                                 SerialPort.STOPBITS_1,
+                                 SerialPort.PARITY_NONE);
+            
+            serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | 
+                                          SerialPort.FLOWCONTROL_RTSCTS_OUT);
+            
+            serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+            // writing string to port
+            serialPort.writeString("Hurrah!!!");
+            
+            System.out.println("String wrote to port, waiting for response..");
+        }
+        catch (SerialPortException ex) {
+            System.out.println("Error in writing data to port: " + ex);
+        }
+ 
+    }
+    
+    // receiving response from port
+    private static class PortReader implements SerialPortEventListener {
 
-						System.out.println(s);
-						
-					
-					} catch (SerialPortException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				}
-				
-			});
-			
-			
-		} catch (SerialPortException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-
-
-	public static void main(String[] args) {
-		
-		String portlist[] = SerialPortList.getPortNames();
-		
-		for(int i =0; i<portlist.length;i++) { 
-			System.out.println(portlist[i]);
-		}
-		demo obj = new demo();
-		obj.connect(portlist[0]);
-	
-
-		
-		
-		
-		
-		
-
-	}
-
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            if(event.isRXCHAR() && event.getEventValue() > 0) {
+                try {
+                  
+                    String receivedData = serialPort.readString(event.getEventValue());
+                    System.out.println("Received response from port: " + receivedData);
+                   
+                }
+                catch (SerialPortException ex) {
+                    System.out.println("Error in receiving response from port: " + ex);
+                }
+            }
+        }
+    }
 }
-
