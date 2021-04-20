fee5b4240f8ea5392af02575d67f2b1f03e8549e
Jurgen van den Berg <jurgenberg4@gmail.com>
2021-04-20 14:50:35 +0200

    code toegevoegd voor keypad en rfid

---
+++ code/KeypadenRFID/keypad_en_uid_simple/keypad_en_uid_simple.ino
@@ -0,0 +1,73 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Keypad.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+
+#define ROW_NUM 4
+#define COLUMN_NUM 4
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1','2','3', 'A'},
+  {'4','5','6', 'B'},
+  {'7','8','9', 'C'},
+  {'*','0','#', 'D'}
+};
+
+byte pin_rows[ROW_NUM] = {9, 8, 7 , 6};
+byte pin_column[COLUMN_NUM] = {5, 4, 3, 2};
+
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+void loop() {
+  if(rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    printKey();
+  }
+  
+  char key = keypad.getKey();
+  if(key) {
+    Serial.print("key: ");
+    Serial.println(key);
+  }
+}
+
+void printKey() {
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    //Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  // Store NUID into nuidPICC array
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+
+  Serial.print("uid:");
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(rfid.uid.uidByte[i], DEC);
+  }
+  Serial.println();
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
---
+++ code/RFID/rfid_uid_only_simple/rfid_uid_only_simple.ino
@@ -0,0 +1,52 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+void loop() {
+  if(rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    printKey();
+  }
+
+}
+
+void printKey() {
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    //Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  // Store NUID into nuidPICC array
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+
+  Serial.print("uid:");
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(rfid.uid.uidByte[i], DEC);
+  }
+  Serial.println();
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
